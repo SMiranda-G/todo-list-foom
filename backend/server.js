8a7fdfc5ab54a3206d22fd030f10a5cb34cddfea
SMiranda-G <selpinamiranda15704@gmail.com>
2026-07-31 20:02:19 +0800

    added express

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('API is working');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
