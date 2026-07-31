4229200cb9bdf9b0b0f6c7f894a544f7328d21e3
SMiranda-G <selpinamiranda15704@gmail.com>
2026-07-31 20:16:49 +0800

    Setting Up Database

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const db = require('./src/models');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Test route
 app.get('/', (req, res) => {
   res.send('API is working');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+async function startServer() {
+  try {
+    await db.sequelize.authenticate();
+    console.log('Database connected!');
+    await db.sequelize.sync();
+    app.listen(PORT, () => {
+      console.log(`Server running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error('Unable to start server:', error);
+  }
+}
+
+startServer();
